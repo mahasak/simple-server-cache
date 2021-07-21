018e56c31c448f178ea61d362f31240a20716bee
Max Pijittum <mpijittum@fb.com>
2021-07-21 12:27:06 +0800

    formatting

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const express = require('express');
 const app = express();
 const cron = require('node-cron');
-const NodeCache = require( "node-cache" );
+const NodeCache = require("node-cache");
 const myCache = new NodeCache();
 
 app.use(express.json());
 
-cron.schedule('*/1 * * * * *', function() {
-  value = myCache.get( "myKey" );
-  if ( value == undefined ){
-      // handle miss!
-      console.log("Cannot find key from cache, setting one");
-      const obj = { my: "Special", variable: 42 };
-      const obj2 = { my: "other special", variable: 1337 };
-      
-      const success = myCache.mset([
-          {key: "myKey", val: obj, ttl: 10000},
-          {key: "myKey2", val: obj2},
-      ])
+cron.schedule('*/1 * * * * *', function () {
+  value = myCache.get("myKey");
+  if (value == undefined) {
+    // handle miss!
+    console.log("Cannot find key from cache, setting one");
+    const obj = { my: "Special", variable: 42 };
+    const obj2 = { my: "other special", variable: 1337 };
+
+    const success = myCache.mset([
+      { key: "myKey", val: obj, ttl: 10000 },
+      { key: "myKey2", val: obj2 },
+    ])
   } else {
     console.log("Value from cache" + JSON.stringify(value));
   }
@@ -25,4 +25,4 @@ cron.schedule('*/1 * * * * *', function() {
 
 app.listen(3000, () => {
   console.log("Listening on port 3000");
-  });
+});
